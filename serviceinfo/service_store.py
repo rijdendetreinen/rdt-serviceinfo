deb93dfcbee286f598ebb665ac8dbfc65d7cb5ad
Geert Wirken <geert@gwirken.nl>
2015-03-19 01:44:06 +0900

    Retrieving service details from Redis

--- serviceinfo/service_store.py
+++ serviceinfo/service_store.py
@@ -1,4 +1,5 @@
 import redis
+from serviceinfo.data import Service, ServiceStop
 
 class ServiceStore(object):
     TYPE_SCHEDULED = 'scheduled'
@@ -28,6 +29,7 @@ class ServiceStore(object):
         self.redis.delete('%s:stops' % key_prefix)
 
         # TODO: metadata like train type, etc.
+        # Enforce lowercase stop codes
 
         # Add stops:
         for stop in service.stops:
@@ -36,14 +38,22 @@ class ServiceStore(object):
                 continue
 
             # Add service and metadata
-            self.redis.lpush('%s:stops' % key_prefix, stop.stop_code)
-            self.redis.hset('%s:stops:%s' % (key_prefix, stop.stop_code), 'arrival_time', stop.arrival_time)
-            self.redis.hset('%s:stops:%s' % (key_prefix, stop.stop_code), 'departure_time', stop.departure_time)
-            self.redis.hset('%s:stops:%s' % (key_prefix, stop.stop_code), 'arrival_platform', stop.arrival_platform)
-            self.redis.hset('%s:stops:%s' % (key_prefix, stop.stop_code), 'departure_platform', stop.departure_platform)
-            self.redis.hset('%s:stops:%s' % (key_prefix, stop.stop_code), 'arrival_delay', stop.arrival_delay)
-            self.redis.hset('%s:stops:%s' % (key_prefix, stop.stop_code), 'departure_delay', stop.departure_delay)
-            self.redis.hset('%s:stops:%s' % (key_prefix, stop.stop_code), 'station_name', stop.stop_name)
+            self.redis.rpush('%s:stops' % key_prefix, stop.stop_code)
+
+            # Add the following data:
+            stop_data = {'arrival_time': stop.arrival_time,
+                         'departure_time': stop.departure_time,
+                         'arrival_platform': stop.arrival_platform,
+                         'departure_platform': stop.departure_platform,
+                         'arrival_delay': stop.arrival_delay,
+                         'departure_delay': stop.departure_delay,
+                         'stop_name': stop.stop_name}
+
+            for k,v in stop_data.iteritems():
+                if v == None:
+                    stop_data[k] = ''
+
+            self.redis.hmset('%s:stops:%s' % (key_prefix, stop.stop_code), stop_data)
 
 
     def store_services(self, services, type):
@@ -55,3 +65,35 @@ class ServiceStore(object):
 
         for service in services:
             self.store_service(service, type)
+
+
+    def get_service(self, service_id, type):
+        service = Service()
+
+        # Check whether service exists:
+        if self.redis.sismember('services:%s' % type, service_id) == False:
+            return None
+
+        service.service_id = service_id
+
+        # Determine Redis key prefix:
+        key_prefix = 'service:%s:%s' % (type, service_id)
+
+        # Get stops:
+        stops = self.redis.lrange('%s:stops' % key_prefix, 0, -1)
+
+        for stop in stops:
+            service_stop = ServiceStop(stop)
+
+            data = self.redis.hgetall('%s:stops:%s' % (key_prefix, stop))
+            service_stop.arrival_time = data['arrival_time']
+            service_stop.departure_time = data['departure_time']
+            service_stop.arrival_time = data['arrival_time']
+            service_stop.arrival_platform = data['arrival_platform']
+            service_stop.departure_platform = data['departure_platform']
+            service_stop.arrival_delay = data['arrival_delay']
+            service_stop.departure_delay = data['departure_delay']
+
+            service.stops.append(service_stop)
+
+        return service
