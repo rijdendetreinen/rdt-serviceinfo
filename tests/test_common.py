64cf915b827375629a39ad76f2fbbda48c6616b5
Geert Wirken <geert@gwirken.nl>
2015-06-03 00:18:30 +0200

    Corrected unit test for serviceinfo.common module

--- tests/test_common.py
+++ tests/test_common.py
@@ -2,35 +2,32 @@ import serviceinfo.common as common
 
 import unittest
 
+
 class CommonTest(unittest.TestCase):
     def test_load_config(self):
         config = common.load_config("config/serviceinfo.yaml.dist")
 
         self.assertTrue({'scheduler', 'arnu_source'}.issubset(config), "Config dict missing important keys")
 
-
     def test_load_config_nonexisting(self):
         with self.assertRaises(SystemExit) as cm:
             common.load_config("config/non_existing_config_file.yaml")
-
 
     def test_load_config_invalid(self):
         with self.assertRaises(SystemExit) as cm:
             common.load_config("README.md")
 
-
     def test_load_config_(self):
         with self.assertRaises(SystemExit) as cm:
             common.load_config("config/non_existing_config_file.yaml")
 
-
     def test_setup_logging(self):
-		common.configuration['logging']['log_config'] = 'non_existing_config_file'
-		common.setup_logging()
+        common.configuration['logging']['log_config'] = 'non_existing_config_file'
+        common.setup_logging("unit-test")
 
-		common.configuration['logging']['log_config'] = 'config/logging.yaml.dist'
-		common.setup_logging()
+        common.configuration['logging']['log_config'] = 'config/logging.yaml.dist'
+        common.setup_logging("unit-test")
 
 
-if __name__ == '__main__': #
+if __name__ == '__main__':  #
     unittest.main()
