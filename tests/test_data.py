d1d65afc1ba5fd9019b50516a498df611c4a623c
Geert Wirken <geert@gwirken.nl>
2015-06-18 00:38:05 +0200

    Added test case for stop.get_departure_platform()

--- tests/test_data.py
+++ tests/test_data.py
@@ -20,7 +20,6 @@ class ServiceTest(unittest.TestCase):
         self.assertEquals(service.get_destination(), stop2)
         self.assertEquals(service.get_destination_str(), "asd")
 
-
     def test_service_servicedate(self):
         service = data.Service()
         service.servicenumber = 1234
@@ -28,13 +27,26 @@ class ServiceTest(unittest.TestCase):
 
         self.assertEquals(service.get_servicedate_str(), "2015-04-01")
 
-
     def test_stop_repr(self):
         stop = data.ServiceStop("ut")
         stop.stop_name = "Utrecht Centraal"
 
         self.assertEquals(repr(stop), "<ServiceStop @ ut>")
 
+    def test_stop_platform(self):
+        stop = data.ServiceStop("ledn")
+
+        stop.scheduled_departure_platform = None
+        stop.actual_departure_platform = None
+        self.assertIsNone(stop.get_departure_platform())
+
+        stop.scheduled_departure_platform = "9a"
+        stop.actual_departure_platform = None
+        self.assertEquals(stop.get_departure_platform(), "9a")
+
+        stop.scheduled_departure_platform = "9a"
+        stop.actual_departure_platform = "8a"
+        self.assertEquals(stop.get_departure_platform(), "8a")
 
     def test_service_repr(self):
         service = data.Service()
@@ -52,7 +64,6 @@ class ServiceTest(unittest.TestCase):
         service.stops.append(stop)
 
         self.assertEquals(repr(service), "<Service i999 / IC9876-asd @ 2015-04-01 [2 stops]>")
-
 
 if __name__ == '__main__': #
     unittest.main()
