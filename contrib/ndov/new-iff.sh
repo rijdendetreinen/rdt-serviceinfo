505a9a477c4f7880ac46352855ac2111b5a3d647
Geert Wirken <geert@gwirken.nl>
2016-10-16 23:52:59 +0200

    Make new-iff.sh work in every directory

--- contrib/ndov/new-iff.sh
+++ contrib/ndov/new-iff.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-if [ -f ../../cache/ns-latest.zip ];
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)/../../"
+BASEDIR=`realpath $DIR`
+
+if [ -f $BASEDIR/cache/ns-latest.zip ];
 then
-	mv ../../cache/ns-latest.zip ../../cache/ns-old.zip
+	mv $BASEDIR/cache/ns-latest.zip $BASEDIR/cache/ns-old.zip
 else
-	if [ -f ../../cache/ns-old.zip ];
+	if [ -f $BASEDIR/cache/ns-old.zip ];
 	then
-		touch ../../cache/ns-old.zip
+		touch $BASEDIR/cache/ns-old.zip
 	fi;
 fi;
 
 # Haal nieuwe dataset op:
-wget --quiet http://data.ndovloket.nl/ns/ns-latest.zip -O ../../cache/ns-latest.zip
+wget --quiet http://data.ndovloket.nl/ns/ns-latest.zip -O $BASEDIR/cache/ns-latest.zip
 
 # Vergelijk files:
-diff -u ../../cache/ns-latest.zip ../../cache/ns-old.zip > /dev/null
+diff -u $BASEDIR/cache/ns-latest.zip $BASEDIR/cache/ns-old.zip > /dev/null
 
 if [[ $? -ne 0 ]]; then
 	echo "Nieuwe dataset!"
-	rm ../../cache/dataset/*
+	rm $BASEDIR/cache/dataset/*
 
-	unzip ../../cache/ns-latest.zip -d ../../cache/dataset/
-	rm ../../cache/iff_parsed/*.tsv
-	../../iff-converter.py --input ../../cache/dataset --config ../../config/serviceinfo.yaml --output ../../cache/iff_parsed
-	../../iff-loader.py --parsed_dir ../../cache/iff_parsed --config ../../config/serviceinfo.yaml --truncate_tables
+	unzip $BASEDIR/cache/ns-latest.zip -d $BASEDIR/cache/dataset/
+	rm $BASEDIR/cache/iff_parsed/*.tsv
+	$BASEDIR/iff-converter.py --input $BASEDIR/cache/dataset --config $BASEDIR/config/serviceinfo.yaml --output $BASEDIR/cache/iff_parsed
+	$BASEDIR/iff-loader.py --parsed_dir $BASEDIR/cache/iff_parsed --config $BASEDIR/config/serviceinfo.yaml --truncate_tables
 else 
 	echo "Opgehaalde dataset is gelijk"
 fi;
